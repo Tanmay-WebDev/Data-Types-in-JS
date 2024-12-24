e989f1e9ec798b8fc6be9591770816b544a6fe95
Tanmay-WebDev <umas81261@gmail.com>
2024-12-24 18:52:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const a = 100;
+const b = 200;
+const c = 200;
+const d = null;
+const e =
+
+
+document.write("This is the value of a = b:  "+ (a==b))
+document.write("<br>")
+document.write("This is the value of b = c:  "+ (b==c)) 
+document.write("<br>")
+document.write("This is the value of d:  "+ d)
+document.write("<br>")
+document.write("This is the value of e:  "+ e)
